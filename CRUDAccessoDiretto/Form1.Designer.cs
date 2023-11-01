4f042daa55e7e44881ecab833400981465ab3e2e
MirkoColo <colombomirko06@gmail.com>
2023-11-01 16:12:49 +0100

    Aggiungere i file di progetto.

---
+++ CRUDAccessoDiretto/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CRUDAccessoDiretto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LISTA = new System.Windows.Forms.ListBox();
+            this.NOME = new System.Windows.Forms.TextBox();
+            this.PREZZO = new System.Windows.Forms.TextBox();
+            this.No = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConfermaUpdate = new System.Windows.Forms.Button();
+            this.UPDATE = new System.Windows.Forms.Button();
+            this.DELETE = new System.Windows.Forms.Button();
+            this.CREATE = new System.Windows.Forms.Button();
+            this.QUANTITA = new System.Windows.Forms.Button();
+            this.piuQ = new System.Windows.Forms.Button();
+            this.menoQ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LISTA
+            // 
+            this.LISTA.FormattingEnabled = true;
+            this.LISTA.ItemHeight = 16;
+            this.LISTA.Location = new System.Drawing.Point(795, 119);
+            this.LISTA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LISTA.Name = "LISTA";
+            this.LISTA.Size = new System.Drawing.Size(725, 516);
+            this.LISTA.TabIndex = 0;
+            this.LISTA.SelectedIndexChanged += new System.EventHandler(this.LISTA_SelectedIndexChanged);
+            // 
+            // NOME
+            // 
+            this.NOME.Location = new System.Drawing.Point(220, 191);
+            this.NOME.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NOME.Name = "NOME";
+            this.NOME.Size = new System.Drawing.Size(193, 22);
+            this.NOME.TabIndex = 1;
+            // 
+            // PREZZO
+            // 
+            this.PREZZO.Location = new System.Drawing.Point(220, 256);
+            this.PREZZO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.PREZZO.Name = "PREZZO";
+            this.PREZZO.Size = new System.Drawing.Size(193, 22);
+            this.PREZZO.TabIndex = 2;
+            // 
+            // No
+            // 
+            this.No.AutoSize = true;
+            this.No.Location = new System.Drawing.Point(219, 171);
+            this.No.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(44, 16);
+            this.No.TabIndex = 3;
+            this.No.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(216, 236);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prezzo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ConfermaUpdate
+            // 
+            this.ConfermaUpdate.Location = new System.Drawing.Point(220, 315);
+            this.ConfermaUpdate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ConfermaUpdate.Name = "ConfermaUpdate";
+            this.ConfermaUpdate.Size = new System.Drawing.Size(195, 60);
+            this.ConfermaUpdate.TabIndex = 5;
+            this.ConfermaUpdate.Text = "Conferma Update";
+            this.ConfermaUpdate.UseVisualStyleBackColor = true;
+            this.ConfermaUpdate.Click += new System.EventHandler(this.ConfermaUpdate_Click);
+            // 
+            // UPDATE
+            // 
+            this.UPDATE.Location = new System.Drawing.Point(452, 315);
+            this.UPDATE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UPDATE.Name = "UPDATE";
+            this.UPDATE.Size = new System.Drawing.Size(195, 60);
+            this.UPDATE.TabIndex = 6;
+            this.UPDATE.Text = "UPDATE";
+            this.UPDATE.UseVisualStyleBackColor = true;
+            this.UPDATE.Click += new System.EventHandler(this.UPDATE_Click);
+            // 
+            // DELETE
+            // 
+            this.DELETE.Location = new System.Drawing.Point(452, 412);
+            this.DELETE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DELETE.Name = "DELETE";
+            this.DELETE.Size = new System.Drawing.Size(195, 60);
+            this.DELETE.TabIndex = 7;
+            this.DELETE.Text = "DELETE";
+            this.DELETE.UseVisualStyleBackColor = true;
+            this.DELETE.Click += new System.EventHandler(this.DELETE_Click);
+            // 
+            // CREATE
+            // 
+            this.CREATE.Location = new System.Drawing.Point(452, 214);
+            this.CREATE.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CREATE.Name = "CREATE";
+            this.CREATE.Size = new System.Drawing.Size(195, 60);
+            this.CREATE.TabIndex = 8;
+            this.CREATE.Text = "CREATE";
+            this.CREATE.UseVisualStyleBackColor = true;
+            this.CREATE.Click += new System.EventHandler(this.CREATE_Click);
+            // 
+            // QUANTITA
+            // 
+            this.QUANTITA.Location = new System.Drawing.Point(248, 412);
+            this.QUANTITA.Name = "QUANTITA";
+            this.QUANTITA.Size = new System.Drawing.Size(132, 60);
+            this.QUANTITA.TabIndex = 9;
+            this.QUANTITA.Text = "Quantità";
+            this.QUANTITA.UseVisualStyleBackColor = true;
+            this.QUANTITA.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // piuQ
+            // 
+            this.piuQ.Location = new System.Drawing.Point(248, 478);
+            this.piuQ.Name = "piuQ";
+            this.piuQ.Size = new System.Drawing.Size(55, 37);
+            this.piuQ.TabIndex = 10;
+            this.piuQ.Text = "+";
+            this.piuQ.UseVisualStyleBackColor = true;
+            this.piuQ.Click += new System.EventHandler(this.piuQ_Click);
+            // 
+            // menoQ
+            // 
+            this.menoQ.Location = new System.Drawing.Point(323, 478);
+            this.menoQ.Name = "menoQ";
+            this.menoQ.Size = new System.Drawing.Size(57, 37);
+            this.menoQ.TabIndex = 11;
+            this.menoQ.Text = "-";
+            this.menoQ.UseVisualStyleBackColor = true;
+            this.menoQ.Click += new System.EventHandler(this.menoQ_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1744, 807);
+            this.Controls.Add(this.menoQ);
+            this.Controls.Add(this.piuQ);
+            this.Controls.Add(this.QUANTITA);
+            this.Controls.Add(this.CREATE);
+            this.Controls.Add(this.DELETE);
+            this.Controls.Add(this.UPDATE);
+            this.Controls.Add(this.ConfermaUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.PREZZO);
+            this.Controls.Add(this.NOME);
+            this.Controls.Add(this.LISTA);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LISTA;
+        private System.Windows.Forms.TextBox NOME;
+        private System.Windows.Forms.TextBox PREZZO;
+        private System.Windows.Forms.Label No;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConfermaUpdate;
+        private System.Windows.Forms.Button UPDATE;
+        private System.Windows.Forms.Button DELETE;
+        private System.Windows.Forms.Button CREATE;
+        private System.Windows.Forms.Button QUANTITA;
+        private System.Windows.Forms.Button piuQ;
+        private System.Windows.Forms.Button menoQ;
+    }
+}
+
---
+++ CRUDAccessoDiretto/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDAccessoDiretto
+{
+    public partial class Form1 : Form
+    {
+        public struct Prodotto
+        {
+            public string nome;
+            public float prezzo;
+            public int quantita;
+        }
+        public Prodotto[] p;
+        public int dim;
+        public int quanti = 1;
+        public int dimCodProdotto;
+        bool Confermadelete = false;
+        bool Confermaupdate = false;
+        bool ConfermaQuantita = false;
+        public int[] CodProdotto;
+        public string Nome = "@"; public string Prezzo = "@"; public int Quantita = 0;  // campi per record vuoto
+        public int size = 64;  // lunghezza (30+30+4) del record PREFISSATA
+        public int NumeroRecord;
+        public string riga;
+        public byte[] strInByte;
+        public Form1()
+        {
+            InitializeComponent();
+            dim = 0;
+            dimCodProdotto = 0;
+            p = new Prodotto[dim];
+            CodProdotto = new int[dimCodProdotto];
+            DELETE.Enabled = false;
+            UPDATE.Enabled = false;
+            QUANTITA.Enabled = false;
+            piuQ.Enabled = false;
+            menoQ.Enabled = false;
+            ConfermaUpdate.Enabled = false;
+            CreaFile();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CREATE_Click(object sender, EventArgs e)
+        {
+            bool controllo = ControlloInserimento(NOME.Text, PREZZO.Text);
+
+            if (NOME.Text == "" && PREZZO.Text == "")
+            {
+                MessageBox.Show("Inserire il nome e il prezzo del prodotto");
+            }
+            else if (NOME.Text == "")
+            {
+                MessageBox.Show("Inserire il nome del prodotto");
+            }
+            else if (PREZZO.Text == "")
+            {
+                MessageBox.Show("Inserire il prezzo del prodotto");
+            }
+            else if (controllo == true)
+            {
+                //if (ControlloQuantita(NOME.Text, float.Parse(PREZZO.Text)) == false)
+                Array.Resize(ref p, p.Length + 1);
+                p[dim].nome = NOME.Text;
+                p[dim].prezzo = float.Parse(PREZZO.Text);
+                p[dim].quantita = 1;
+                dim++;
+                //MessageBox.Show($"Il prodotto {NOME.Text} al prezzo di {PREZZO.Text} euro è stato aggiunto al carrello");
+                NOME.Text = "";
+                PREZZO.Text = "";
+                AggiornaLista();
+                UPDATE.Enabled = true;
+                DELETE.Enabled = true;
+                QUANTITA.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Il prodotto non è stato inserito");
+                NOME.Text = "";
+                PREZZO.Text = "";
+            }
+        }
+
+        private void UPDATE_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selezionare il prodotto da cancellare e poi Inserire il nome e il prezzo del nuovo prodotto");
+            Confermaupdate = true;
+            CREATE.Enabled = false;
+            UPDATE.Enabled = false;
+            DELETE.Enabled = false; 
+            ConfermaUpdate.Enabled = true;
+        }
+
+        private void ConfermaUpdate_Click(object sender, EventArgs e)
+        {
+            bool controllo = ControlloInserimento(NOME.Text, PREZZO.Text);
+
+            if (NOME.Text == "" && PREZZO.Text == "")
+            {
+                MessageBox.Show("Inserire il nome e il prezzo del nuovo prodotto");
+            }
+            else if (NOME.Text == "")
+            {
+                MessageBox.Show("Inserire il nome del nuovo prodotto");
+            }
+            else if (PREZZO.Text == "")
+            {
+                MessageBox.Show("Inserire il prezzo del nuovo prodotto");
+            }
+            else if (controllo == true)
+            {
+                int indiceLista = LISTA.SelectedIndex;
+                p[indiceLista].nome = NOME.Text;
+                p[indiceLista].prezzo = float.Parse(PREZZO.Text);
+                AggiornaLista();
+                ConfermaUpdate.Enabled = false;
+                NOME.Text = "";
+                PREZZO.Text = "";
+                CREATE.Enabled = true;
+                UPDATE.Enabled = true;
+                DELETE.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Il prodotto non è stato inserito");
+                NOME.Text = "";
+                PREZZO.Text = "";
+            }
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selezionare il prodotto da cancellare");
+            Confermadelete = true;
+            CREATE.Enabled = false;
+            UPDATE.Enabled = false;
+            QUANTITA.Enabled = false;
+            ConfermaUpdate.Enabled = false;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selezionare il prodotto di cui si vuole cambiare la quantità");
+            ConfermaQuantita = true;
+            CREATE.Enabled = false;
+            UPDATE.Enabled = false;
+            DELETE.Enabled = false;
+        }
+        private void piuQ_Click(object sender, EventArgs e)
+        {
+            p[LISTA.SelectedIndex].quantita += 1;
+            AggiornaLista();
+            piuQ.Enabled = false;
+            menoQ.Enabled = false;
+            CREATE.Enabled = true;
+            UPDATE.Enabled = true;
+            DELETE.Enabled = true;
+        }
+
+        private void menoQ_Click(object sender, EventArgs e)
+        {
+            if (p[LISTA.SelectedIndex].quantita != 1)
+            {
+                p[LISTA.SelectedIndex].quantita--;
+                AggiornaLista();              
+            }
+            menoQ.Enabled = false;
+            piuQ.Enabled = false;
+            CREATE.Enabled = true;
+            UPDATE.Enabled = true;
+            DELETE.Enabled = true;
+        }
+
+        private void LISTA_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Confermadelete == true)
+            {
+                Confermadelete = false;
+                for (int i = LISTA.SelectedIndex; i < p.Length - 1; i++)
+                {
+                    p[i].nome = p[i + 1].nome;
+                    p[i].prezzo = p[i + 1].prezzo;
+                }
+                Array.Resize(ref p, p.Length - 1);
+                dim--;
+                AggiornaLista();
+                if (dim == 0)
+                {
+                    UPDATE.Enabled = false;
+                    DELETE.Enabled = false;
+                    CREATE.Enabled = true;
+                }
+                else
+                {
+                    CREATE.Enabled = true;
+                    UPDATE.Enabled = true;
+                    DELETE.Enabled = true;
+                }
+            }
+            //UPDATE
+            else if (Confermaupdate == true)
+            {
+                ConfermaUpdate.Enabled = true;
+                UPDATE.Enabled = false;
+            }
+            else if (ConfermaQuantita == true)
+            {
+                ConfermaQuantita = false;
+                piuQ.Enabled = true;
+                menoQ.Enabled = true;
+            }
+        }
+        //FUNZIONI DI SERVIZIO
+
+        public void AggiornaLista() 
+        {
+            LISTA.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                LISTA.Items.Add($"{p[i].nome};{p[i].prezzo}euro;{p[i].quantita}");
+            }
+        }
+        public bool ControlloInserimento(string nome, string prezzo)
+        {
+            bool uscitaPrezzo = false;
+            if (prezzo.All(char.IsNumber))
+            {
+                uscitaPrezzo = true;
+            }
+            return uscitaPrezzo;
+        }
+
+        
+
+        /*
+        public bool ControlloQuantita(string nome, float prezzo)
+        {
+            bool esiste = false;
+
+            for(int i = 0; i < dim; i++)
+            {
+                if (p[i].nome == nome && p[i].prezzo == prezzo)
+                {
+                    
+                    p[i].quantita++;
+                    esiste = true;
+                    break;
+                }
+            }
+            return esiste;
+        }
+        */
+
+        public void CreaFile()
+        {
+            FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryWriter f_out = new BinaryWriter(f_in_out);
+            riga = Nome.PadRight(30) + Prezzo.PadRight(30) + (Quantita.ToString()).PadRight(4);
+            strInByte = Encoding.Default.GetBytes(riga);
+            for (int i = 1; i <= 100; i++)
+                f_out.Write(strInByte);  
+        }
+    }
+}
