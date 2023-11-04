61f3ebc613a54f1542942acf47331886ada40353
MirkoColo <colombomirko06@gmail.com>
2023-11-04 20:39:55 +0100

    Cancellazione Fisica

--- CRUDAccessoDiretto/Form1.cs
+++ CRUDAccessoDiretto/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace CRUDAccessoDiretto
 {
@@ -23,12 +24,15 @@ namespace CRUDAccessoDiretto
         }
         public Prodotto[] p;
         public int dim;
+        public int dim1 = 0;
         public int codProd = 1;
         bool Confermadelete = false;
         bool Confermaupdate = false;
         bool ConfermaQuantita = false;
+        public string cancellazione = ""; //f-fisica ; l-logica
+        public int indiceCanc = -1;
         public int[] CodProdotto;
-        public string Nome = "@"; public int Prezzo = 0; public int Quantita = 0;  // campi per record vuoto
+        public string Nome = "@"; public int Prezzo = 0; public int Quantita = 0; public int Cancellato = 0;  // campi per record vuoto
         public int size = 64;  // lunghezza (30+30+4) del record PREFISSATA
         public int NumeroRecord;
         public string riga;
@@ -73,7 +77,6 @@ namespace CRUDAccessoDiretto
             }
             else if (controllo == true)
             {
-                //if (ControlloQuantita(NOME.Text, float.Parse(PREZZO.Text)) == false)
                 Array.Resize(ref p, p.Length + 1);
                 p[dim].nome = NOME.Text;
                 p[dim].quantita = 1;
@@ -83,7 +86,7 @@ namespace CRUDAccessoDiretto
                 FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
                 BinaryWriter f_out = new BinaryWriter(f_in_out);
 
-                riga = NOME.Text.PadRight(30) + PREZZO.Text.PadRight(30) + Convert.ToString(p[dim].quantita).PadRight(4);
+                riga = NOME.Text.PadRight(30) + PREZZO.Text.PadRight(30) + Convert.ToString(Quantita + 1).PadRight(2) + Convert.ToString(Cancellato).PadRight(2);
                 strInByte = Encoding.Default.GetBytes(riga);
                 f_out.BaseStream.Seek((p[dim].CodProdotto - 1) * size, SeekOrigin.Begin);
                 f_out.Write(strInByte);
@@ -91,10 +94,10 @@ namespace CRUDAccessoDiretto
                 f_out.Close();
                 f_in_out.Close();
 
-                Nome = "@"; Prezzo = 0; Quantita = 0;
+                Nome = "@"; Prezzo = 0; Quantita = 0; Cancellato = 0;
                 codProd++;
                 dim++;
-                //MessageBox.Show($"Il prodotto {NOME.Text} al prezzo di {PREZZO.Text} euro è stato aggiunto al carrello");
+                dim1++;
                 NOME.Text = "";
                 PREZZO.Text = "";
                 AggiornaLista();
@@ -158,17 +161,79 @@ namespace CRUDAccessoDiretto
 
         private void DELETE_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Selezionare il prodotto da cancellare");
-            Confermadelete = true;
-            CREATE.Enabled = false;
-            UPDATE.Enabled = false;
-            QUANTITA.Enabled = false;
-            ConfermaUpdate.Enabled = false;
+
+            InputDelete();
+            if (indiceCanc == -1)
+            {
+                MessageBox.Show("Il prodotto da cancellare non è stato trovato");
+            }
+            else
+            {
+                InputCancellazione();
+                if (cancellazione == "l" || cancellazione == "f")
+                {
+                    Confermadelete = true;
+                    CREATE.Enabled = false;
+                    UPDATE.Enabled = false;
+                    QUANTITA.Enabled = false;
+                    ConfermaUpdate.Enabled = false;
+                    dim1--;
+                    FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    BinaryWriter f_out = new BinaryWriter(f_in_out);
+
+                    if (cancellazione == "f")
+                    {
+                        p[indiceCanc].nome = "";
+                        p[indiceCanc].cancellato = true;
+                        riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + Convert.ToString(Quantita).PadRight(2) + Convert.ToString(Cancellato).PadRight(2);
+                        strInByte = Encoding.Default.GetBytes(riga);
+
+                        f_out.BaseStream.Seek((p[indiceCanc].CodProdotto - 1) * size, 0);
+                        f_out.Write(strInByte);
+
+                        f_out.Close();
+                        f_in_out.Close();
+                        indiceCanc = -1;
+                        AggiornaLista();
+                    }
+                    if (cancellazione == "l")
+                    {
+                        p[indiceCanc].cancellato = true;
+
+                        string a = "1".PadRight(2);
+                        strInByte = Encoding.Default.GetBytes(a);
+
+                        f_out.BaseStream.Seek((p[indiceCanc].CodProdotto - 1) * size + 62, SeekOrigin.Begin);
+                        f_out.Write(strInByte);
+
+                        f_out.Close();
+                        f_in_out.Close();
+
+                        AggiornaLista();
+
+                    }
+                    if (dim1 <= 0)
+                    {
+                        UPDATE.Enabled = false;
+                        DELETE.Enabled = false;
+                        CREATE.Enabled = true;
+                    }
+                    else
+                    {
+                        CREATE.Enabled = true;
+                        UPDATE.Enabled = true;
+                        DELETE.Enabled = true;
+                    }
+
+
+                }
+            }
+            
         }
 
-        
 
-        private void button1_Click(object sender, EventArgs e) //QUANTITA BUTTON
+
+            private void button1_Click(object sender, EventArgs e) //QUANTITA BUTTON
         {
             MessageBox.Show("Selezionare il prodotto di cui si vuole cambiare la quantità");
             ConfermaQuantita = true;
@@ -203,45 +268,9 @@ namespace CRUDAccessoDiretto
 
         private void LISTA_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (Confermadelete == true)
-            {
-                Confermadelete = false;
-                /*for (int i = LISTA.SelectedIndex; i < p.Length - 1; i++)
-                {
-                    p[i].nome = p[i + 1].nome;
-                }
-                Array.Resize(ref p, p.Length - 1);
-                dim--;               
-                */
-                p[LISTA.SelectedIndex].cancellato = false;
-                FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-                BinaryWriter f_out = new BinaryWriter(f_in_out);
-
-                riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + Quantita.ToString().PadRight(4);
-                strInByte = Encoding.Default.GetBytes(riga);
-                f_out.BaseStream.Seek((p[LISTA.SelectedIndex].CodProdotto - 1) * size, 0);
-                f_out.Write(strInByte);
-
-                f_out.Close();
-                f_in_out.Close();
-
-                AggiornaLista();
-
-                if (dim == 0)
-                {
-                    UPDATE.Enabled = false;
-                    DELETE.Enabled = false;
-                    CREATE.Enabled = true;
-                }
-                else
-                {
-                    CREATE.Enabled = true;
-                    UPDATE.Enabled = true;
-                    DELETE.Enabled = true;
-                }
-            }
+                       
             //UPDATE
-            else if (Confermaupdate == true)
+            if (Confermaupdate == true)
             {
                 ConfermaUpdate.Enabled = true;
                 UPDATE.Enabled = false;
@@ -260,7 +289,7 @@ namespace CRUDAccessoDiretto
             FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             BinaryReader f_in = new BinaryReader(f_in_out);
             LISTA.Items.Clear();
-            riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + Convert.ToString(Quantita).PadRight(4);
+            riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + Convert.ToString(Quantita).PadRight(2) + Convert.ToString(Cancellato).PadRight(2);
             byte[] br;
             f_in.BaseStream.Seek(0, SeekOrigin.Begin);
 
@@ -270,14 +299,16 @@ namespace CRUDAccessoDiretto
                 Nome = Encoding.ASCII.GetString(br, 0, br.Length);
                 br = f_in.ReadBytes(30);
                 Prezzo = int.Parse(Encoding.ASCII.GetString(br, 0, br.Length));
-                br = f_in.ReadBytes(4);
+                br = f_in.ReadBytes(2);
                 Quantita = int.Parse(Encoding.ASCII.GetString(br, 0, br.Length));
-                if (Nome[0] != '@')
+                br = f_in.ReadBytes(2);
+                Cancellato = int.Parse(Encoding.ASCII.GetString(br, 0, br.Length));
+                if (Nome[0] != '@' && Cancellato != 1)
                 {                         
-                    LISTA.Items.Add($"{Nome.Trim()};{Prezzo.ToString().Trim()}euro;{Quantita.ToString().Trim()};{p[i].CodProdotto}");
+                    LISTA.Items.Add($"{Nome.Trim()};{Prezzo.ToString().Trim()}euro;{Quantita.ToString().Trim()};{Cancellato.ToString().Trim()}");
                 }               
             }
-            Nome = "@"; Prezzo = 0; Quantita = 0;
+            Nome = "@"; Prezzo = 0; Quantita = 0; Cancellato = 0;
             f_in.Close();
             f_in_out.Close();
         }
@@ -316,7 +347,7 @@ namespace CRUDAccessoDiretto
         {
             FileStream f_in_out = new FileStream("Carrello.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             BinaryWriter f_out = new BinaryWriter(f_in_out);
-            riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + (Quantita.ToString()).PadRight(4);
+            riga = Nome.PadRight(30) + Prezzo.ToString().PadRight(30) + Convert.ToString(Quantita).PadRight(2) + Convert.ToString(Cancellato).PadRight(2); ;
             strInByte = Encoding.Default.GetBytes(riga);
             for (int i = 1; i <= 100; i++)
                 f_out.Write(strInByte);  
@@ -324,5 +355,61 @@ namespace CRUDAccessoDiretto
             f_out.Close();
             f_in_out.Close();
         }
-    }
+
+        //GESTIONE INPUT CON VISUAL BASIC
+        public string InputCancellazione()
+        {
+            string message //ISTRUZIONI FORNITE ALL'UTENTE
+                , title, //TITOLO MESSAGGIO
+                defaultValue; //MESSAGGIO DI BASE
+            bool controllo = false;
+            message = "Inserire f per la cancellazione fisica o l per la cancellazione logica";
+            title = "Input Cancellazione";
+            defaultValue = "";
+            do
+            {
+                cancellazione = Interaction.InputBox(message, title, defaultValue);
+                if (cancellazione == "")
+                {
+                    controllo = true;
+                }
+                else if (cancellazione == "l")
+                {
+                    cancellazione = "l";
+                    controllo = true;
+                }
+                else if (cancellazione == "f")
+                {
+                    cancellazione = "f";
+                    controllo = true;
+                }
+                else
+                {
+                    MessageBox.Show(message);
+                }
+            } while (controllo == false);
+
+
+            return cancellazione;
+        }
+
+        public void InputDelete()
+        {
+            string message, title, defaultValue; 
+            message = "Inserire il nome del prodotto da cancellare";
+            title = "Input Delete";
+            defaultValue = "";
+            string prodotto = Interaction.InputBox(message, title, defaultValue); 
+
+            for(int i = 0; i < dim; i++)
+            {
+                if (p[i].nome == prodotto)
+                {
+                    indiceCanc = i;
+                }
+            }
+        }
+
+
+    }    
 }
